565d3f123eaecd81ccbdb8ae6cfa1ac394c54903
xufeixiang <xufeixiang@ainanjing.org.cn>
2024-02-16 11:05:48 +0800

    feat: add recoverMiddleware for http request.

--- files/server/start.go
+++ files/server/start.go
@@ -6,7 +6,6 @@ import (
 	"custom-go/pkg/types"
 	"custom-go/pkg/utils"
 	"custom-go/pkg/wgpb"
-	"errors"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/labstack/gommon/log"
@@ -79,7 +78,7 @@ func configureWunderGraphServer() *echo.Echo {
 	plugins.BuildDefaultInternalClient(internalQueries, internalMutations)
 
 	registerOnce := &sync.Once{}
-	e.Use(recoverMiddleware, func(next echo.HandlerFunc) echo.HandlerFunc {
+	e.Use(middleware.Recover(), func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			registerOnce.Do(func() {
 				for _, registeredHook := range base.GetRegisteredHookArr() {
@@ -176,22 +175,4 @@ func startServer() error {
 	}
 
 	return nil
-}
-
-func recoverMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
-	return func(c echo.Context) (err error) {
-		defer func() {
-			if e := recover(); e != nil {
-				switch t := e.(type) {
-				case string:
-					err = errors.New(t)
-				case error:
-					err = t
-				default:
-					err = errors.New("unknown error")
-				}
-			}
-		}()
-		return next(c)
-	}
 }
